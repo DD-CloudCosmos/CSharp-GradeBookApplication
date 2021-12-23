5a0766b74f25dd2b663c4e6e1eb226924d15e395
DD-CloudCosmos <david.deceuninck@cloudcosmos.be>
2021-12-23 18:52:02 +0100

    Create GradeBookType.cs
    GradeBookType

---
+++ GradeBook/Enums/GradeBookType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.Enums
+{
+  enum GradeBookType
+  {
+    Standard, Ranked, ESNU, OneToFour, SixPoint
+  }
+}
